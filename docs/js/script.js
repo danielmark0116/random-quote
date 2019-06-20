c408c55ffbd1fe99d7e56b0e83a1cd44aa57e72b
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-06-20 14:49:46 +0200

    Finish app logic

---
+++ docs/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // here goes the code
+
+  console.log('test');
+});
---
+++ js/script.js
@@ -0,0 +1,49 @@
+const quoteUrl =
+  'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+const tweetUrl = 'https://twitter.com/intent/tweet?text=';
+const corsFix = 'https://cors-anywhere.herokuapp.com/';
+
+function generateQuote() {
+  document.querySelector('#result-quote').innerHTML = 'Loading...';
+  document.querySelector('#result-author').innerHTML = '';
+  fetch(`${corsFix}${quoteUrl}`, { cache: 'no-store' })
+    .then(res => res.json())
+
+    .then(data => {
+      generateTweet(data[0]);
+    })
+    .catch(err => {
+      console.log(err);
+      document.querySelector('#result-quote').innerHTML = 'Error';
+    });
+}
+
+function generateTweet(input) {
+  const quoteContent = input.content;
+  let quoteAuthor = input.title;
+  const rawText = document.createElement('div');
+  rawText.innerHTML = quoteContent;
+  if (!quoteAuthor) {
+    quoteAuthor = 'Unknown Author';
+  }
+  const tweetContent = `Quote of the day: "${rawText.innerText.trim()}" by ${quoteAuthor}`;
+
+  if (tweetContent.length > 140) {
+    generateQuote();
+  } else {
+    document.querySelector('#result-quote').innerHTML = rawText.innerText;
+    document.querySelector('#result-author').innerHTML = quoteAuthor;
+    document
+      .querySelector('#tweet')
+      .setAttribute('href', `${tweetUrl}${tweetContent}`);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  generateQuote();
+  document
+    .querySelector('#generate-quote')
+    .addEventListener('click', function() {
+      generateQuote();
+    });
+});
